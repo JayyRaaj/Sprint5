b49407d14b034c97ddd1de76d0424396c734cad8
Jay Raj <iamgjraj@gmail.com>
2024-10-07 20:16:23 -0500

    Add Inverse Kinematics Script

---
+++ Assets/Scripts/IKManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKManager : MonoBehaviour
+{
+    //root of the armature
+    public Joint m_root;
+
+    //end effector
+    public Joint m_end;
+
+    public GameObject m_target;
+
+    public float m_threshold = 0.05f;
+
+    
+
+    float CalculateSlope(Joint _joint)
+    {
+        float deltaTheta = 0.01f;
+        float distance1 = GetDistance(m_end.transform.position, m_target.transform.position);
+
+        // Rotate joint by deltaTheta
+        _joint.transform.Rotate(Vector3.up, deltaTheta);  // Adjust axis if needed
+
+        float distance2 = GetDistance(m_end.transform.position, m_target.transform.position);
+
+        // Rotate joint back by -deltaTheta
+        _joint.transform.Rotate(Vector3.up, -deltaTheta);  // Adjust axis if needed
+
+        return (distance2 - distance1) / deltaTheta;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetDistance(m_end.transform.position, m_target.transform.position) > m_threshold)
+        {
+            float slope = CalculateSlope(m_root);
+            m_root.transform.Rotate(Vector3.up, -slope); // Adjust axis if needed
+        }
+    }
+
+    float GetDistance(Vector3 _point1, Vector3 _point2)
+    {
+        return Vector3.Distance(_point1, _point2);
+    }
+}
